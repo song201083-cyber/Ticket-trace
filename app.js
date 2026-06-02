ebca97837d7844b7853d3bcd126344e893042123
song201083-cyber <song201083@gmail.com>
2026-06-02 10:58:11 +0800

    Initialize app.js with configuration settings

---
+++ app.js
@@ -0,0 +1,41 @@
+{
+  "pages": [
+    "pages/index/index",
+    "pages/predict/predict",
+    "pages/hot/hot",
+    "pages/task/task"
+  ],
+  "window": {
+    "navigationBarTitleText": "票迹",
+    "navigationBarBackgroundColor": "#fafafa",
+    "navigationBarTextStyle": "black",
+    "backgroundColor": "#fafafa"
+  },
+  "tabBar": {
+    "custom": true,
+    "color": "#8e8e8e",
+    "selectedColor": "#262626",
+    "backgroundColor": "#ffffff",
+    "borderStyle": "white",
+    "list": [
+      {
+        "pagePath": "pages/index/index",
+        "text": "监控"
+      },
+      {
+        "pagePath": "pages/predict/predict",
+        "text": "预测"
+      },
+      {
+        "pagePath": "pages/hot/hot",
+        "text": "热门"
+      },
+      {
+        "pagePath": "pages/task/task",
+        "text": "任务"
+      }
+    ]
+  },
+  "style": "v2",
+  "sitemapLocation": "sitemap.json"
+}
